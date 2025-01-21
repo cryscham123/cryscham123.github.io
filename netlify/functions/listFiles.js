d304e406e99a1ef578c83d6d8d23d0febf47be6d
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2025-01-21 06:18:32 +0000

    Built site for gh-pages

---
+++ netlify/functions/listFiles.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async (event, context) => {
+    try {
+         const directoryPath = path.join(process.cwd(), 'public/docs');
+         const files = fs.readdirSync(directoryPath);
+
+         return {
+             statusCode: 200,
+             body: JSON.stringify({ files }),
+         };
+       } catch (error) {
+         return {
+             statusCode: 500,
+             body: JSON.stringify({ error: 'Internal Server Error' }),
+         };
+     }
+};
